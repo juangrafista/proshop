7945a0793752dfa4a0cc684fc67726e09ef8695a
juangrafista <juangrafista@gmail.com>
2020-10-23 19:06:10 -0400

    installed colors and added colors to our console messages

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,9 +8,9 @@ const connectDB = async () => {
       useCreateIndex: true,
     })
 
-    console.log(`mongoDB Connected: ${conn.connection.host}`)
+    console.log(`mongoDB Connected: ${conn.connection.host}`.cyan.underline)
   } catch (error) {
-    console.error(`Error: ${error.message}`)
+    console.error(`Error: ${error.message}`.red.underline.bold)
     process.exit(1)
   }
 }
