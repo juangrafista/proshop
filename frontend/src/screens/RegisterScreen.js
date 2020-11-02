0fa2aec115231d63202e307619f1d38c7a2f26bb
juangrafista <juangrafista@gmail.com>
2020-11-01 20:45:44 -0500

    added shipping address and save address

--- frontend/src/screens/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen.js
@@ -1,14 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import {
-  Form,
-  Button,
-  Row,
-  Col,
-  FormGroup,
-  FormLabel,
-  FormControl,
-} from 'react-bootstrap'
+import { Form, Button, Row, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
@@ -51,42 +43,42 @@ const RegisterScreen = ({ location, history }) => {
       {error && <Message variant='danger'>{error}</Message>}
       {loading && <Loader />}
       <Form onSubmit={submitHandler}>
-        <FormGroup controlId='name'>
-          <FormLabel>Name</FormLabel>
-          <FormControl
+        <Form.Group controlId='name'>
+          <Form.Label>Name</Form.Label>
+          <Form.Control
             type='name'
             placeholder='Enter name'
             value={name}
             onChange={(e) => setName(e.target.value)}
-          ></FormControl>
-        </FormGroup>
-        <FormGroup controlId='email'>
-          <FormLabel>Email Address</FormLabel>
-          <FormControl
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group controlId='email'>
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
             type='email'
             placeholder='Enter email'
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-          ></FormControl>
-        </FormGroup>
-        <FormGroup controlId='password'>
-          <FormLabel>Password</FormLabel>
-          <FormControl
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group controlId='password'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control
             type='password'
             placeholder='Enter Password'
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-          ></FormControl>
-        </FormGroup>
-        <FormGroup controlId='confirmPassword'>
-          <FormLabel>Confirm Password</FormLabel>
-          <FormControl
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group controlId='confirmPassword'>
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control
             type='password'
             placeholder='Confirm Password'
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
-          ></FormControl>
-        </FormGroup>
+          ></Form.Control>
+        </Form.Group>
         <Button type='submit' variant='primary'>
           Register
         </Button>
